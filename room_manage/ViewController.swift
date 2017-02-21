587a279b8fd3bc7a9b1f01575027aef1fffe3704
Vishesh Mistry <mistry.1@iitj.ac.in>
2017-02-21 18:25:33 +0530

    Initial Commit

---
+++ room_manage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  room_manage
+//
+//  Created by VISHESH MISTRY on 21/02/17.
+//  Copyright © 2017 VISHESH MISTRY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
